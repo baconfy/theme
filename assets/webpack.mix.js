95290a16e1cb8e0fc0c786599d4e6a3ad0204836
Renato Dehnhardt <renatotkd@gmail.com>
2020-02-19 14:18:08 +0100

    Change files location

--- assets/webpack.mix.js
+++ assets/webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
 mix.sass('resources/assets/sass/app.scss', 'dist')
-mix.copy('resources/assets/images/*', 'dist/img')
+mix.copy('resources/assets/images', 'dist/img')
 mix.js('resources/assets/js/app.js', 'dist')
