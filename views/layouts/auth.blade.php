e4c10d99addd2d1ed1f9c973f77c0c025c760889
Renato Dehnhardt <renatotkd@gmail.com>
2020-02-27 11:41:44 +0100

    Navigation style

--- views/layouts/auth.blade.php
+++ views/layouts/auth.blade.php
@@ -3,15 +3,15 @@
 @section('page')
 <div class="container-fluid">
     <div class="row no-gutter">
-        <div class="d-none d-md-flex d-lg-flex d-xl-flex align-content-end flex-wrap col-md-6 col-lg-8 image" style="background-image: url('{{ config('ui.guest.background') }}')">
+        <div class="d-none d-md-flex d-lg-flex d-xl-flex align-content-end flex-wrap col-md-6 col-lg-8 empty-space" style="background-image: url('{{ config('ui.guest.background') }}')">
             <div class="slogan">{{ config('ui.guest.text') }}</div>
         </div>
 
         <div class="col-md-6 col-lg-4">
             <div class="content d-flex align-items-center">
-                <div class="container">
+                <div class="container-fluid">
                     <div class="row pb-5">
-                        <div class="col-md-9 col-lg-8 mx-auto">
+                        <div class="col">
                             <div class="logo">
                                 <img src="{{ config('ui.brand-vertical') }}" class="img-fluid" />
                             </div>
