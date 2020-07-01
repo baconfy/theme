d232fdfb878b3c373b161c7c6195e750d6105e40
Renato Dehnhardt <renatotkd@gmail.com>
2020-07-01 18:43:16 +0200

    Bootstrap 5 Alpha 1 - Navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,18 @@
-@extends('ui::layouts.empty', ['class' => 'bg-gray-100'])
+@extends('ui::layouts.empty', ['class' => 'app'])
 
 @section('page')
-    <input type="checkbox" class="hidden" id="navigation-toggle"/>
+    <input type="checkbox" class="d-none" id="navigation-toggle"/>
 
     <aside class="navigation">
         <section class="header">
-            <img src="{{ config('ui.brand-vertical') }}" class="brand"/>
+            <img src="{{ config('ui.brand-vertical') }}" class="img-fluid"/>
         </section>
 
-        <section class="content h-screen">
+        <section class="module">
+            MODULE
+        </section>
+
+        <section class="content flex-fill">
             <ul>
                 <li><a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
                 <li><a href="#"><i class="fab fa-buromobelexperte"></i> Widgets</a></li>
@@ -16,12 +20,11 @@
         </section>
 
         <section class="footer">
-            <a href="#" title="{{ __('ui::navigation.support') }}"><i class="fas fa-question-circle"></i></a>
-            <a href="#" title="{{ __('ui::navigation.logout') }}"><i class="fas fa-sign-out-alt"></i></a>
+            <a class="btn btn-block btn-sm btn-secondary" href="{{ route('logout') }}" title="{{ __('ui::navigation.logout') }}"><i class="fas fa-power-off mr-1"></i> Sign out</a>
         </section>
     </aside>
 
-    <main class="main">
+    <main class="content">
         <label for="navigation-toggle">Open/Close</label>
 
         @yield('content')
