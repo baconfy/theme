824d40e4cd8ce342b4d77eeef94053ae9934194d
Renato Dehnhardt <renatotkd@gmail.com>
2020-10-02 17:10:37 +0200

    Password Confirm

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,23 +1,48 @@
-@extends('ui::layouts.auth')
+@extends('ui::layouts.empty', ['class' => 'flex h-screen justify-center'])
 
 @section('title', __('ui::auth.confirm.title'))
 @section('welcome', __('ui::auth.confirm.welcome'))
 
-@section('content')
-    <form method="POST" action="{{ route('password.confirm') }}">
-        @csrf
+@section('page')
+    <div class="flex flex-col justify-between w-full lg:w-1/3">
+        <header class="p-4">
+            <img src="{{ config('ui.brand-icon') }}" class="w-10 h-10" />
+        </header>
 
-        <label class="block">
-            <span class="form-label">{{ __('ui::auth.login.password') }}</span>
-            <input type="password" name="password" class="form-input mb-1" required/>
+        <main class="px-10 lg:px-20">
+            <h1 class="font-serif leading-tight text-4xl">@yield('title')</h1>
+            <h2 class="mt-3 mb-15 text-sm text-gray-400">@yield('welcome')</h2>
 
-            @if (Route::has('password.request'))
-                <a class="form-label text-right mb-6" href="{{ route('password.request') }}">{{ __('ui::auth.login.forgot-password') }}</a>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
-        </label>
 
-        <button class="btn btn-primary" type="submit">{{ __('ui::auth.confirm.action') }}</button>
-    </form>
+            <form method="POST" action="{{ route('password.confirm') }}">
+                @csrf
 
-    <a href="{{ url()->previous() }}" class="block mt-10 text-center text-sm">{!! __('ui::auth.confirm.back')  !!}</a>
+                <label class="block">
+                    <span class="form-label">{{ __('ui::auth.login.password') }}</span>
+                    <input type="password" name="password" class="form-input mb-1" required/>
+
+                    @if (Route::has('password.request'))
+                        <a class="form-label text-right mb-6" href="{{ route('password.request') }}">{{ __('ui::auth.login.forgot-password') }}</a>
+                    @endif
+                </label>
+
+                <button class="btn btn-primary" type="submit">{{ __('ui::auth.confirm.action') }}</button>
+            </form>
+
+            <a href="{{ url()->previous() }}" class="block mt-10 text-center text-sm">{!! __('ui::auth.confirm.back')  !!}</a>
+        </main>
+
+        <footer class="flex flex-col lg:flex-row items-center justify-end py-1 px-2 mt-20">
+            <div class="leading-tight text-xs tracking-wide text-gray-500">©{{ date('Y') }} - All Rights Reserved</div>
+        </footer>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,18 +2,24 @@
 
 @section('page')
     <div class="h-screen flex text-gray-600">
-        <input type="checkbox" class="hidden" id="by-navigation-toggle" />
-
+        <input type="checkbox" class="hidden" id="by-navigation-toggle"/>
         <div class="by-navigation-overlay"></div>
-
         <nav class="by-navigation">
             @include('ui::layouts.partials.sidebar')
         </nav>
 
         <main class="by-content">
             @include('ui::layouts.partials.header')
-
             @yield('content')
         </main>
+
+        <input type="checkbox" class="hidden" id="by-modules-toggle"/>
+        <div class="by-modules flex fixed w-full content-center flex-wrap bg-white h-full z-20">
+            <div class="w-full p-2">
+                <div class="text-center p-2">
+                    <i class="fas fa-th-large mr-2"></i>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
