fa6921693158a07141289560890ea2def0a33bc1
Renato Dehnhardt <renatotkd@gmail.com>
2021-03-10 11:07:21 +0100

    FIX Auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,8 @@
-<x-empty-layout class="guest flex h-screen" page-title="{{ $attributes['page-title'] }}">
+<x-empty-layout class="flex h-screen" page-title="{{ $attributes['page-title'] }}">
+    <input type="checkbox" class="hidden" id="by-notifications-toggle" />
+    <input type="checkbox" class="hidden" id="by-navigation-toggle" />
+
+    <x-navigation></x-navigation>
+
     {{ $slot }}
 </x-empty-layout>
